d84aa66153b0530f5df0032c24f7a01e959319b9
Russell Gordon <rgordon@rsgc.on.ca>
2016-03-06 09:44:49 -0500

    Initial thoughts on problem

---
+++ SnakesAndLadders.playground/Contents.swift
@@ -0,0 +1,12 @@
+// Russell Gordon
+
+// Notes
+// 
+// This is basically a situation of repeated addition, with some special cases:
+//
+// a snake, where the position will be reduced
+// a ladder, where the position will be increased
+//
+// I note that to win, you must roll exactly the sum needed to get to 100
+// (not past 100, that doesn't count)
+
